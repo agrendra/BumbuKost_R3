504c0ffd406e883f0f7bf484f35af6ff443c06d4
agrendra <agerf11@gmail.com>
2018-01-18 23:15:59 +0700

    Bismillah ga ada yang ga mungkin kalo Allah udah Ridho #Wisuda_2018

---
+++ app/src/main/java/com/example/agrendra/bumbukost_r3/CustomListAdapter2.java
@@ -0,0 +1,48 @@
+package com.example.agrendra.bumbukost_r3;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.agrendra.bumbukost_r3.R;
+
+/**
+ * Created by sanriomisintaro on 10/27/17.
+ */
+
+public class CustomListAdapter2 extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final String[] itemname;
+    //private final String[] descriptionItem;
+    private final Integer[] imgid;
+
+    public CustomListAdapter2(Activity context, String[] itemname, Integer[] imgid) {
+        super(context, R.layout.list_data2, itemname);
+        // TODO Auto-generated constructor stub
+
+        this.context=context;
+        this.itemname=itemname;
+        //this.descriptionItem=descriptionItem;
+        this.imgid=imgid;
+    }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.list_data2, null,true);
+
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.txtJudul);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.imgId);
+
+        //dari instansiasi diatas, dimasukkan data yang diambil dari array yang ada di MainActivity
+        txtTitle.setText(itemname[position]);
+        imageView.setImageResource(imgid[position]);
+        //extratxt.setText("Description "+descriptionItem[position]);
+        return rowView;
+
+    };
+}
